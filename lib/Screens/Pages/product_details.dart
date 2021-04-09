b94343ecf5b0d52bc55b061d387c801b6f1d7bf8
nidhi2802 <nidhigajjar2000@gmail.com>
2021-04-09 18:58:52 +0530

    Product Details Update

--- lib/Screens/Pages/product_details.dart
+++ lib/Screens/Pages/product_details.dart
@@ -72,7 +72,22 @@ class _ProductDetailsState extends State<ProductDetails> {
           ),
           Row(
             children: <Widget>[
-              Expanded(child: MaterialButton(onPressed: (){},
+              Expanded(child: MaterialButton(onPressed: (){
+                showDialog(context: context,
+                    builder: (context){
+                  return new AlertDialog(
+                    title: new Text("Packet Size"),
+                    content: new Text("Select the quantity"),
+                    actions: <Widget>[
+                      new MaterialButton(onPressed: (){
+                        Navigator.of(context).pop(context);
+                      },
+                      child: new Text("Close"),
+                      )
+                    ],
+                  );
+                    });
+              },
               color: Colors.white,
               textColor: Colors.grey,
                 elevation: 0.2,
@@ -84,7 +99,22 @@ class _ProductDetailsState extends State<ProductDetails> {
                       child: new Icon(Icons.arrow_drop_down)),
                 ],
               ),)),
-              Expanded(child: MaterialButton(onPressed: (){},
+              Expanded(child: MaterialButton(onPressed: (){
+                showDialog(context: context,
+                    builder: (context){
+                      return new AlertDialog(
+                        title: new Text("Packets"),
+                        content: new Text("Select the total packets"),
+                        actions: <Widget>[
+                          new MaterialButton(onPressed: (){
+                            Navigator.of(context).pop(context);
+                          },
+                            child: new Text("Close"),
+                          )
+                        ],
+                      );
+                    });
+              },
                 color: Colors.white,
                 textColor: Colors.grey,
                 elevation: 0.2,
@@ -111,6 +141,31 @@ class _ProductDetailsState extends State<ProductDetails> {
               new IconButton(icon: Icon(Icons.add_shopping_cart, color: kPrimaryLightColor,), onPressed: (){}),
 
               new IconButton(icon: Icon(Icons.favorite_border, color: kPrimaryLightColor,), onPressed: (){}),
+            ],
+          ),
+
+          Divider(),
+
+          new ListTile(
+            title: new Text("Description"),
+            subtitle: new Text("Considered as the most commonly grown apples in India, these apples have light red skin, juicy and crunchy flesh. We source the best apples with residue and wax free peel from trusted growers."),
+          ),
+          Divider(),
+
+          new Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+              child: new Text("Product Name", style: TextStyle(color: Colors.grey),),),
+              Padding(padding: EdgeInsets.all(5.0),
+              child: new Text(widget.product_detail_name),)
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text("Product Brand", style: TextStyle(color: Colors.grey),),),
+              Padding(padding: EdgeInsets.all(5.0),
+                child: new Text("Brand Name"),)
             ],
           )
         ],
